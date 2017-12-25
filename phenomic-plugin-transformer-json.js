a7966ccc03bf398081133f429d4f6d4ab2fd7f01
Khoa Nguyen <khoa02031995@gmail.com>
2017-12-26 04:44:10 +0700

    Simplify phenomic configs

---
+++ phenomic-plugin-transformer-json.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const unified = require("unified");
+const markdown = require("remark-parse");
+const highlight = require("remark-highlight.js");
+const toc = require("remark-toc");
+const remarkRehype = require("remark-rehype");
+const raw = require("rehype-raw");
+const slug = require("rehype-slug");
+const autoLinkHeadings = require("rehype-autolink-headings");
+const html = require("rehype-stringify");
+const rehype2react = require("rehype-react");
+
+function mdTransform(config, body) {
+  const processor = unified()
+    .use(markdown)
+    .use(highlight)
+    .use(remarkRehype, { allowDangerousHTML: true })
+    .use(raw)
+    .use(slug)
+    .use(autoLinkHeadings)
+    .use(html);
+
+  return processor.processSync(body);
+}
+
+function transformJSONFile({ config, file, contents }) {
+  const data = Object.assign({}, JSON.parse(contents.toString()), {
+    path: path.basename(file.name, ".json"),
+  });
+  data.body = mdTransform(config, data.body).contents;
+
+  const partial = _objectWithoutProperties(data, ["body"]);
+
+  return {
+    data,
+    partial,
+  };
+}
+
+module.exports = function() {
+  return {
+    name: "@phenomic/plugin-transform-json",
+    supportedFileTypes: ["json"],
+    transform: transformJSONFile,
+  };
+};
+
+function _objectWithoutProperties(obj, keys) {
+  var target = {};
+  for (var i in obj) {
+    if (keys.indexOf(i) >= 0) continue;
+    if (!Object.prototype.hasOwnProperty.call(obj, i)) continue;
+    target[i] = obj[i];
+  }
+  return target;
+}
--- phenomic.config.js
+++ phenomic.config.js
@@ -1,5 +1,17 @@
-const path = require("path");
+const bundlerWebpack = require("@phenomic/plugin-bundler-webpack");
+const rendererReact = require("@phenomic/plugin-renderer-react");
+const collectorFiles = require("@phenomic/plugin-collector-files");
+const transformJson = require("./transformJson");
+const apiRelatedContent = require("@phenomic/plugin-api-related-content");
+const publicAssets = require("@phenomic/plugin-public-assets");
 
 module.exports = {
-  presets: [path.resolve(__dirname, "phenomic-preset.js")],
+  plugins: [
+    bundlerWebpack,
+    rendererReact,
+    transformJson,
+    collectorFiles,
+    apiRelatedContent,
+    publicAssets,
+  ],
 };
