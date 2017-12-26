d00c7a7b6bcbf87dbf40c5ef0ed2e2b7a8c20adc
Khoa Nguyen <khoa02031995@gmail.com>
2017-12-26 22:57:10 +0700

    Add meta tags to Html

--- Html.js
+++ Html.js
@@ -1,13 +1,27 @@
 import * as React from "react";
 import { renderStaticOptimized } from "glamor/server";
+import Head from "react-helmet";
 
 export default ({ App, render }) => {
   const { html, Main, State, Script } = render(<App />);
   const { css, ids } = renderStaticOptimized(() => html);
-
+  const helmet = Head.renderStatic();
   return (
-    <html>
+    <html {...helmet.htmlAttributes.toComponent()}>
       <head>
+        <meta charset="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {helmet.meta.toComponent()}
+        {helmet.title.toComponent()}
+        {helmet.base.toComponent()}
+        {helmet.link.toComponent()}
+        {helmet.style.toComponent()}
+        {helmet.script.toComponent()}
+        {helmet.noscript.toComponent()}
         <style dangerouslySetInnerHTML={{ __html: css }} />
       </head>
       <body>
